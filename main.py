f06ad5791e2626ef6d653f7b928373ffe55b7292
moravalubos <moravalubos@gmail.com>
2026-09-17 20:45:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hasdasdasdello World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
